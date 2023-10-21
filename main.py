c018e2c2efd91507af5afb63df1671229650ff2c
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-22 01:26:00 +0400

    initial functions added

--- main.py
+++ main.py
@@ -12,16 +12,40 @@ def compare(first, second):
 
 def participiant_info(index1):
     print(
-        f"Compare A: {data[index1]['name']}, a {data[index1]['description']}, from {data[index1]['country']}"
+        f"{data[index1]['name']}, a {data[index1]['description']}, from {data[index1]['country']}"
     )
     return data[index1]["follower_count"]
 
 
-f_index = randint(0, len(data) - 1)
-s_index = randint(0, len(data) - 1)
-f_followers = 0
-s_followers = 0
+def checking_answer(answer, your_guess, point):
+    if answer == your_guess:
+        print("You win!")
+        return point + 1
+    else:
+        print("You lose")
+        return False
 
-f_followers = participiant_info(f_index)
-s_followers = participiant_info(s_index)
-print(f"ff-{f_followers} ss-{s_followers}")
+
+def game():
+    should_continue = True
+    score = 0
+    s_index = randint(0, len(data) - 1)
+    while should_continue:
+        f_index = s_index
+        print(logo)
+        s_index = randint(0, len(data) - 1)
+        f_followers = 0
+        s_followers = 0
+        print(f"Compare A:", end=" ")
+        f_followers = participiant_info(f_index)
+        print(vs)
+        print(f"Against B:", end=" ")
+        s_followers = participiant_info(s_index)
+        print(f"first followers {f_followers} second followers {s_followers}")
+        answer = compare(f_followers, s_followers)
+        your_option = input("Who has more followers? Type 'A' or 'B': ")
+        score = checking_answer(answer, your_option, score)
+        print(f"You're right. Current score: {score}")
+
+
+game()
