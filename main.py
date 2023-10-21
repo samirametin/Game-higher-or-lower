500d3551980f987f32e21f7dfd348a22c3e4deec
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-22 02:23:24 +0400

    debugging

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from icons import logo, vs
 from game_data import data
-from random import randint
+from random import choice
 
 
 def compare(first, second):
@@ -10,30 +10,30 @@ def compare(first, second):
         return "B"
 
 
-def participiant_info(index1):
+def participiant_info(data_item):
     print(
-        f"{data[index1]['name']}, a {data[index1]['description']}, from {data[index1]['country']}"
+        f"{data_item['name']}, a {data_item['description']}, from {data_item['country']}"
     )
-    return data[index1]["follower_count"]
+    return data_item["follower_count"]
 
 
 def game():
     should_continue = True
     score = 0
-    s_index = randint(0, len(data) - 1)
+    second = choice(data)
     while should_continue:
-        f_index = s_index
+        first = second
         print(logo)
-        s_index = randint(0, len(data) - 1)
+        second = choice(data)
         f_followers = 0
         s_followers = 0
         print(f"Compare A:", end=" ")
-        f_followers = participiant_info(f_index)
+        f_followers = participiant_info(first)
         print(vs)
         print(f"Against B:", end=" ")
-        s_followers = participiant_info(s_index)
+        s_followers = participiant_info(second)
         answer = compare(f_followers, s_followers)
-        your_option = input("Who has more followers? Type 'A' or 'B': ")
+        your_option = input("Who has more followers? Type 'A' or 'B': ").lower()
         if answer == your_option:
             score += 1
             print(f"You're right. Current score: {score}")
